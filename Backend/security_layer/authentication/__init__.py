523471d9ba8a5c378612568f1706b38d4103ae37
ahmedelhadi17776 <Ahmed221101032@nmu.edu.eg>
2025-02-12 02:36:13 +0200

    refactor: update package-lock version and remove unused security and database models

--- Backend/security_layer/authentication/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Authentication service package."""
-from .auth_service import AuthService
-from .dependencies import get_current_user, oauth2_scheme
-
-__all__ = [
-    "AuthService",
-    "get_current_user",
-    "oauth2_scheme"
-]
--- Backend/security_layer/authorization/__init__.py
+++
@@ -1,9 +0,0 @@
-from .permission_service import PermissionService
-from .role_service import RoleService
-from .dependencies import require_role
-
-__all__ = [
-    "PermissionService",
-    "RoleService",
-    "require_role"
-]
